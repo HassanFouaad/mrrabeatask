d098b1263411409f131a8654a40e5e2a89c62ea2
HassanFouaad <mrhassanfouad1@gmail.com>
2020-09-10 08:32:51 +0200

    ok

--- test/api/user/post.js
+++ test/api/user/post.js
@@ -37,3 +37,40 @@ describe("POST /api/signup", () => {
       .catch((err) => done(err));
   });
 });
+
+const expect = require("chai").expect;
+const request = require("supertest");
+
+const app = require("../../../index.js");
+const conn = require("../../../config/index");
+
+describe("POST /api/signin", () => {
+  before((done) => {
+    conn
+      .connect(process.env.DATABASEURL)
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+  after((done) => {
+    conn
+      .disconnect()
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+
+  it("OK, Signing up", (done) => {
+    request(app)
+      .post("/api/signin")
+      .send({
+        username: "thisisTester",
+        password: "123456",
+      })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property("user");
+        expect(body).to.contain.property("token");
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
